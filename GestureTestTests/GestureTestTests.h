129642e8a19a41d610df01b91a1958c27ff5ad31
David Kapp <davekapp@gmail.com>
2012-02-12 21:47:54 -0700

    Initial Commit

---
+++ GestureTestTests/GestureTestTests.h
@@ -0,0 +1,13 @@
+//
+//  GestureTestTests.h
+//  GestureTestTests
+//
+//  Created by David Kapp on 2/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GestureTestTests : SenTestCase
+
+@end
---
+++ GestureTestTests/GestureTestTests.m
@@ -0,0 +1,32 @@
+//
+//  GestureTestTests.m
+//  GestureTestTests
+//
+//  Created by David Kapp on 2/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GestureTestTests.h"
+
+@implementation GestureTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GestureTestTests");
+}
+
+@end
